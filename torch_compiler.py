db1b30eb8c85ae088cf611fe10e0e1cc95b71813
Rodri <rodgarcas98@gmail.com>
2025-02-20 09:53:46 +0000

    MODIFIED: torch compile functions

--- torch_compiler.py
+++ torch_compiler.py
@@ -1,3 +1,4 @@
+# https://pytorch.org/docs/stable/torch.compiler.html
 import torch
 import torch.nn.functional as F
 from torch.profiler import profile, record_function, ProfilerActivity
@@ -5,7 +6,7 @@ import time
 import sys
 import os
 
-# Define a scaled dot-product attention function.
+# Define a scaled dot-product attention function. CHANGED: for cuda graph to fusion kernels, we need to make the op more complex 
 def attention(q, k, v):
     # q, k, v shape: [batch_size, num_heads, seq_length, d_k]
     d_k = q.size(-1)
@@ -13,7 +14,12 @@ def attention(q, k, v):
     scores = torch.matmul(q, k.transpose(-2, -1)) / (d_k ** 0.5)
     attn = torch.softmax(scores, dim=-1)
     # Multiply the attention weights by the values.
-    return torch.matmul(attn, v)
+    mul = torch.matmul(attn, v)
+
+    # Complication from https://pytorch.org/docs/stable/torch.compiler_get_started.html
+    a = torch.sin(mul)
+    b = torch.cos(a)
+    return b
 
 # Create sample tensors.
 batch_size = 32
@@ -49,14 +55,14 @@ def attn_seq(q,k,v):
     print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
     if CHROME_PROFILE: prof.export_chrome_trace("basic_example_async.json")
 
-# Sequential approach for torch.compiler
+# Sequential approach for torch.compiler -> TorchInductor -> Triton backend
 def attn_compiler_seq(q,k,v):
     """
     Perform number_heads x attn_opperations using torch.compile
     """
     torch.cuda.empty_cache()
 
-    compiled_attention = torch.compile(attention)
+    compiled_attention = torch.compile(attention, backend="inductor")
 
     # warmup
     _ = compiled_attention(q, k, v)
@@ -74,5 +80,9 @@ def attn_compiler_seq(q,k,v):
     print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
     if CHROME_PROFILE: prof.export_chrome_trace("basic_example_torch_compile_async.json")
 
+# CUDA graphs
+def attn_compiler_graphs(q,k,v):
+    #TODO
+    pass
 attn_seq(q,k,v) # CPU: 8.168ms GPU: 465us op_time: 3.049 ms
 attn_compiler_seq(q,k,v) # CPU: 1.895ms GPU: 394us op_time: 2.078ms
